80b995994711ea2ab28498a026164cc051a970b8
KostyaMerenkov <KonstantinLikes@gmail.com>
2021-11-05 20:57:48 +1000

    Refactoring

--- colorpicker/src/main/java/com/sergiobelda/compose/colorpicker/ColorPicker.kt
+++ colorpicker/src/main/java/com/sergiobelda/compose/colorpicker/ColorPicker.kt
@@ -27,19 +27,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.contentColorFor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.MainAxisAlignment
+import com.google.accompanist.flowlayout.SizeMode
 
-@OptIn(ExperimentalLayout::class)
 @Composable
 fun ColorPicker(
-    items: List<Color?>,
+    colors: List<Color?>,
     selectedColor: Color?,
     onColorSelected: (color: Color?) -> Unit,
     modifier: Modifier = Modifier
@@ -51,7 +54,7 @@ fun ColorPicker(
             crossAxisSpacing = 4.dp,
             mainAxisSpacing = 4.dp
         ) {
-            items.distinct().forEach { color ->
+            colors.distinct().forEach { color ->
                 ColorItem(
                     selected = color == selectedColor,
                     color = color,
@@ -72,22 +75,32 @@ fun ColorItem(
         modifier = Modifier
             .padding(4.dp)
             .clip(CircleShape)
-            .preferredSize(40.dp)
+            .requiredSize(40.dp)
             .clickable(onClick = onClick)
     ) {
         if (color != null) {
             // Transparent background pattern
-            Box(modifier = Modifier.width(20.dp).fillMaxHeight().background(grey400))
+            Box(
+                modifier = Modifier
+                    .width(20.dp)
+                    .fillMaxHeight()
+                    .background(grey400)
+            )
             // Color indicator
             val colorModifier =
                 if (color.luminance() < 0.1 || color.luminance() > 0.9) {
-                    Modifier.fillMaxSize().background(color).border(
-                        width = 1.dp,
-                        color = MaterialTheme.colors.onSurface,
-                        shape = CircleShape
-                    )
+                    Modifier
+                        .fillMaxSize()
+                        .background(color)
+                        .border(
+                            width = 1.dp,
+                            color = MaterialTheme.colors.onSurface,
+                            shape = CircleShape
+                        )
                 } else {
-                    Modifier.fillMaxSize().background(color)
+                    Modifier
+                        .fillMaxSize()
+                        .background(color)
                 }
             Box(
                 modifier = colorModifier
@@ -95,6 +108,7 @@ fun ColorItem(
                 if (selected) {
                     Icon(
                         Icons.Default.Check,
+                        contentDescription = Icons.Default.Check.name,
                         tint = if (color.luminance() < 0.5) Color.White else Color.Black,
                         modifier = Modifier.align(Alignment.Center)
                     )
@@ -111,7 +125,8 @@ fun ColorItem(
             }
             // Color null indicator
             Icon(
-                vectorResource(R.drawable.ic_color_off_24dp),
+                painterResource(R.drawable.ic_color_off_24dp),
+                contentDescription = Icons.Default.Clear.name,
                 modifier = Modifier.align(Alignment.Center),
                 tint = contentColorFor(MaterialTheme.colors.surface)
             )
