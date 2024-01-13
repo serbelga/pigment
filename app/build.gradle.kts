4f8a96d6be64ba2d97a28c99137881ed07432785
Sergio Belda <sergio.belda@gruposantander.com>
2024-01-13 17:58:43 +0100

    Update repository: - Upgrade dependencies - Migrate groovy to kts - Update project - Apply spotless

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.sergiobelda.pigment-spotless")
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "dev.sergiobelda.pigment.catalog"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    kotlin {
+        jvmToolchain(17)
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
+    }
+    namespace = "dev.sergiobelda.pigment.catalog"
+}
+
+dependencies {
+    implementation(project(":pigment"))
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.ui:ui-tooling:1.5.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ pigment/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("dev.sergiobelda.pigment-spotless")
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 34
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    kotlin {
+        jvmToolchain(17)
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
+    }
+    namespace = "dev.sergiobelda.pigment"
+}
+
+dependencies {
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.ui:ui-tooling:1.5.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
